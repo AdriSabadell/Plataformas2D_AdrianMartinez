d398bcb9f86fe4873986f3368cca8001faab3edb
AdriSabadell <e.amartinezl@ies-sabadell.cat>
2024-02-13 21:26:15 +0100

    SolucionManMenu
    El botón Exit no funcionaba

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,16 +4,14 @@ var velocidad = 450
 
 func _ready():
 	pass  
+
 func _process(delta):
 	position.x += velocidad * delta
-
 
 func _on_area_entered(area):
 	if area.is_in_group("Enemy"):
 		print("Golpe")
 		queue_free()
-
-
 
 func _on_body_entered(body):
 	if body.is_in_group("Terreno"):
