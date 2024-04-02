eac05f5d87fd8e41c846ab6a5ce7577cf73b0d9c
AdriSabadell <e.amartinezl@ies-sabadell.cat>
2024-04-02 21:18:45 +0200

    Rename niveles

--- Scripts/BulletEnemy.gd
+++ Scripts/BulletEnemy.gd
@@ -10,3 +10,10 @@ func _on_body_entered(body):
 		body.loseLife(position.x)
 		print("Golpe")
 		queue_free()
+	
+	
+
+
+func _on_body_entered_terrain(body):
+	if body.is_in_group("Terreno"):
+		queue_free()
