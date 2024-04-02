dc28719e94ef8c17840def3e0eb0c5f107dd706d
AdriSabadell <e.amartinezl@ies-sabadell.cat>
2024-04-02 21:11:15 +0200

    Level Tutorial

--- Scripts/Enemy2Shoot.gd
+++ Scripts/Enemy2Shoot.gd
@@ -1,35 +1,34 @@
 extends Area2D
 
-# Variables para el enemigo
+
 var velocidad_disparo = 1.0
 var tiempo_entre_disparos = 1.0
 var tiempo_ultimo_disparo = 0.0
 var bala = preload("res://Scenes/BulletEnemy.tscn")
 
-@onready var area_vision = $"Area de vision" # Asigna la referencia al área de visión en el editor
+@onready var area_vision = $"Area de vision" 
 
-# Variable para rastrear si el jugador está dentro del área de visión
 var jugador_en_area_vision = false
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"): # Verifica si el cuerpo es el jugador
+	if body.is_in_group("Player"): 
 		jugador_en_area_vision = true
 		start_shooting()
 		print("EntraJugador")
 		
 
 func _on_body_exited(body):
-	if body.is_in_group("Player"): # Verifica si el cuerpo es el jugador
+	if body.is_in_group("Player"): 
 		jugador_en_area_vision = false
 		print("JugadorSale")
 
-# Función para iniciar el disparo si el jugador está en el área de visión
+
 func start_shooting():
 	if jugador_en_area_vision:
 		set_process(true)
 		print("Llamada")
 
-# Función que se llama en cada frame
+
 func _process(delta):
 	if jugador_en_area_vision and tiempo_ultimo_disparo >= tiempo_entre_disparos:
 		shoot()
@@ -37,14 +36,13 @@ func _process(delta):
 	else:
 		tiempo_ultimo_disparo += delta
 
-# Función para realizar el disparo
+
 func shoot():
 	var nueva_bala = bala.instantiate()
 	var marker_position = $Marker2D.global_position
 	nueva_bala.position = marker_position
 	$SonidoDisparo.playing = true
-	#nueva_bala.global_position = global_position # Posición de la bala igual a la del enemigo
-	get_parent().add_child(nueva_bala) # Añade la bala como hijo del nodo padre del enemigo
+	get_parent().add_child(nueva_bala) 
 	print("Disparo Enemigo")
 
 
